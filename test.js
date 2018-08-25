4ec33a2cbf0ff9a8baf2185ec9d619be639411cc
jaehunpark <setyourmindpark@gmail.com>
2018-08-25 16:15:56 +0900

    update test code

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
 import mongodbPlugin from './dist';
 
 ( async () => {
-    const db = await mongodbPlugin.createModule('mongodb://localhost:27017/example');
+    const db = await mongodbPlugin.createModule('mongodb://localhost:27017/example');    
     const collection = db.collection('hello');
     const insertOne = await collection.insertOne({
-        name: 'jaehunpark'
+        name: 'jaehunpark1'
     });
     // {
     //     result: { n: 1, ok: 1 },
     //     ...
     //     insertedCount: 1,
     //     insertedId: 5b80f7a3a93fda51c1cee7ea
-    // }    
+    // }  
+    // you can handle other databases
+    const db2 = await mongodbPlugin.createModule('mongodb://localhost2:27017/example'); 
+    const collection2 = db2.collection('hello');
+    const insertOne2 = await collection2.insertOne({
+        name: 'jaehunpark2'
+    }); 
 })()
